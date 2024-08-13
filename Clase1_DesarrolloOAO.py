d8745ecc5f0dbf9fa8b19188618b457edec24609
NBello26 <163012023+NBello26@users.noreply.github.com>
2024-08-13 11:51:19 -0400

    Add files via upload

---
+++ Clase1_DesarrolloOAO.py
@@ -0,0 +1,23 @@
+"""
+Martes 10:41am/12:50pm Clase práctica(Ejercicios)
+Viernes 8:31am/ 9:50am Clase Cátedra(Informacion, materia)
+Asistencia: 70%
+
+Parcial 1 -> Clases y atributos 2 (30%) (100% se divide en 80% del parcial y 20% por tareas) (10 de Septiembre)
+    Clases: Generalización de un objeto
+    Atributo: Característica/datos
+    Objeto: Especificación
+
+Parcial 2 -> Herencia, polimorfismo y colecciones (40%) (100% se divide en 80% del parcial y 20% por tareas) (22 de Octubre)
+    Herencia: Atributos en común que se reciben de una clase superior (Optimiza al no crear tantas veces un mismo atributo)
+    
+Parcial 3 -> Interfaz, estructura y validación (30%) (26 de Noviembre)
+    Grupos de 3 integrantes (Presentación semi-formal)
+    Interfaz: Diseño
+    Controlador: Conexiones dentro del programa
+    Modelo: Insertar, eliminar, buscar, actualizar
+
+Programas:
+NetBeans IDE 18/ApacheNetBeans
+MySQL Workbench 8.0 CE
+"""
