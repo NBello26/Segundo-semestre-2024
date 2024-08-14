7e5e4bb764c66bdfe8a58073e9012cfc7035c267
NBello26 <163012023+NBello26@users.noreply.github.com>
2024-08-14 12:16:31 -0400

    Add files via upload

---
+++ Ingles/Clase1_Ingles.py
@@ -0,0 +1,13 @@
+"""
+1.- Presentation
+2.- Email: mu.oyarce@profesor.duoc.cl
+    Profesor: Muriel Oyarce Piraud
+3.- Evaluation:
+    1- Test written 1 (15%) (Units 1-2) (9-14 of September)
+    2- Test written 2 (15%) (Units 3-4) (14-19 of October)
+    3- Test oral 1 (28%) (Units 1-2-3-4) (14-19 of October)
+    4- Test oral 2 (27%) (Units 5-6) (18-23 of November)
+    5- Activity to confirm (15%) (25-30 of November)
+    Total value of 5 notes is 60%, the other 40% it's to Cross Exam (written part 70% and oral part 30%)
+4.- Ebook in Ava -> General resources -> Level 1A Ebook
+"""
