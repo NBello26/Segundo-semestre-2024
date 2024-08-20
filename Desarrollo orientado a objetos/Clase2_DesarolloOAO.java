0808f816ebd104751a8047dd50763fcb1f2d08f7
NBello26 <163012023+NBello26@users.noreply.github.com>
2024-08-20 12:43:15 -0400

    Add files via upload

---
+++ Desarrollo orientado a objetos/Clase2_DesarolloOAO.java
@@ -0,0 +1,64 @@
+/*
+Clase: Molde(planilla) que representara a un conjunto de objetos
+Objeto: Entidades que se pueden asociar a casi cualquier sustantivo (Auto, Casa, Tarjeta, Celular, etc).
+    Atributos: Caracteristicas relevantes del objeto.
+    Comportamientos: Acciones del objeto.
+Instancias:
+*/
+
+/*
+Una mascota que se vacuna en una veterinaria
+Clase: 
+    - Mascota
+        Atributos:
+            - Nombre
+            - Raza
+            - Peso
+            - Sexo
+            - Tamaño
+            - Dueño
+            - Fecha de atención
+            - Alergias
+            - Edad
+        Metodos:
+            - Registrar()
+            - Diagnosticar()
+            - Vacunar()
+            - Hospitalizar()
+    - Atención
+    - Veterinario
+    - Vacuna 
+Objeto: 
+    Perro
+        Atributos:
+                - Nombre: Juan
+                - Raza: Bulldog
+                - Peso: 25kg
+                - Sexo: Macho
+                - Tamaño: 35cm
+                - Dueño: Martin
+                - Fecha atencion: 20/08/2024
+                - Alergias: Ninguna
+                - Edad: 5 años
+            Metodos:
+                - Registrar()
+                - Diagnosticar()
+                - Vacunar()
+                - Hospitalizar()
+    Gato
+        Atributos:
+                    - Nombre: Tsuki
+                    - Raza: No aplica
+                    - Peso: 5kg
+                    - Sexo: Femenino
+                    - Tamaño: 20cm
+                    - Dueño: Nicolás
+                    - Fecha atencion: 20/08/2024
+                    - Alergias: Ninguna
+                    - Edad: 10 meses
+                Metodos:
+                    - Registrar()
+                    - Diagnosticar()
+                    - Vacunar()
+                    - Hospitalizar()
+*/
